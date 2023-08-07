5998aad76cda6612aa0e60a13c1327cc9c2c8526
Omar Aflak <aflakomar@gmail.com>
2023-08-07 16:26:03 +0000

    update add

--- examples/add.asm
+++ examples/add.asm
@@ -1,36 +1,7 @@
-; 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
-
-push 0
-call .fib 1
+push 5
+push 6
+add
 print
 push 10
 printc
 halt
-
-.fib
-store 0
-
-load 0
-push 0
-eq
-jump_if .end
-
-load 0
-push 1
-eq
-jump_if .end
-
-load 0
-push 1
-sub
-call .fib 1
-
-load 0
-push 2
-sub
-call .fib 1
-
-add
-
-.end
-ret
