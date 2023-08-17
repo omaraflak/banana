d4c9526e31ef1b2c6ae0e1f065e49629f0bf7712
Omar <aflakomar@gmail.com>
2023-08-17 09:01:02 +0000

    added tests

--- src/banana_test.cpp
+++ src/banana_test.cpp
@@ -31,4 +31,36 @@ TEST(Print, Variable) {
   EXPECT_EQ("-13\n", exe("long x = -13; print x;"));
 }
 
+TEST(Expression, OrderOfOperations) {
+  EXPECT_EQ("16\n", exe("print 1 + 3 * 5;"));
+  EXPECT_EQ("20\n", exe("print (1 + 3) * 5;"));
+  EXPECT_EQ("4\n", exe("print (1 + 3) * 5 / 5;"));
+  EXPECT_EQ("1\n", exe("print 1 + 6 % 2;"));
+  EXPECT_EQ("3\n", exe("print 1 % 6 + 2;"));
+  EXPECT_EQ("1\n", exe("print 1 % (6 + 2);"));
+  EXPECT_EQ("-11\n", exe("print 1 + 6 - 9 * 2;"));
+  EXPECT_EQ("-1\n", exe("print 0 - 1;"));
+}
+
+TEST(Expression, BooleanOperators) {
+  EXPECT_EQ("false\n", exe("print 1 == 2;"));
+  EXPECT_EQ("true\n", exe("print (1 + 3) * 5 == 20;"));
+  EXPECT_EQ("true\n", exe("print 1 < 2;"));
+  EXPECT_EQ("true\n", exe("print 2 > 0;"));
+  EXPECT_EQ("true\n", exe("print 1 <= 1;"));
+  EXPECT_EQ("true\n", exe("print 1 >= 1;"));
+  EXPECT_EQ("true\n", exe("print 1 != 6;"));
+  EXPECT_EQ("false\n", exe("print 1 != 1;"));
+  EXPECT_EQ("true\n", exe("print true == true;"));
+  EXPECT_EQ("true\n", exe("print false == false;"));
+  EXPECT_EQ("false\n", exe("print true == false;"));
+}
+
+TEST(Expression, SubstituteVariable) {
+  EXPECT_EQ("true\n", exe("int x = 1; print 1 == x;"));
+  EXPECT_EQ("false\n", exe("short x = 25; print x <= 7;"));
+  EXPECT_EQ("0\n", exe("short x = 25; short y = 24; print x - y - 1;"));
+  EXPECT_EQ("3\n", exe("int x = 1; short y = 2; print x + y;"));
+}
+
 }
