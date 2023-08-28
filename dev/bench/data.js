2adbea45cbe1d86a6bda8998dbec0bedfda11463
github-action-benchmark <github@users.noreply.github.com>
2023-08-28 12:47:05 +0000

    add Banana Benchmark (googlecpp) benchmark result for 1a6a523ac3912bf23693cc8cea9a7da3ab3e1b40

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693224196143,
+  "lastUpdate": 1693226825692,
   "repoUrl": "https://github.com/omaraflak/banana",
   "entries": {
     "Banana Benchmark": [
@@ -36,6 +36,48 @@ window.BENCHMARK_DATA = {
             "value": 562491249.9999937,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 561735500 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aflakomar@gmail.com",
+            "name": "Omar",
+            "username": "omaraflak"
+          },
+          "committer": {
+            "email": "aflakomar@gmail.com",
+            "name": "Omar",
+            "username": "omaraflak"
+          },
+          "distinct": true,
+          "id": "1a6a523ac3912bf23693cc8cea9a7da3ab3e1b40",
+          "message": "update readme",
+          "timestamp": "2023-08-28T12:44:28Z",
+          "tree_id": "350cb4accd82785fb8c678983d6a3410c793185e",
+          "url": "https://github.com/omaraflak/banana/commit/1a6a523ac3912bf23693cc8cea9a7da3ab3e1b40"
+        },
+        "date": 1693226824713,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "bm_fib",
+            "value": 1692745543.000001,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1691316200 ns\nthreads: 1"
+          },
+          {
+            "name": "bm_primes",
+            "value": 507665443.00000757,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 507579400 ns\nthreads: 1"
+          },
+          {
+            "name": "bm_while_loop",
+            "value": 9536.993813588299,
+            "unit": "ns/iter",
+            "extra": "iterations: 73225\ncpu: 9536.260839877094 ns\nthreads: 1"
           }
         ]
       }
